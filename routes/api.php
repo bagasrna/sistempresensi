3bc2c428c24a174cbe01946cddc429728c44da93
bagas <bagasrnfull@gmail.com>
2023-11-24 21:59:07 +0700

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\PresenceController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'auth' ,'middleware' => 'guest'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::group(['middleware' => 'user'], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', [AuthController::class, 'getUserProfile']);
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::get('/course/{courseSchedule:id}/schedules', [CourseController::class, 'detail']);
+    Route::post('/presence', [PresenceController::class, 'presence']);
+});
